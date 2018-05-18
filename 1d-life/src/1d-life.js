d107967413051a7110a06087e024c10389dbb663
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-05-18 13:14:18 -0400

    1d-life.js Modified
    Refactored to ternary operations.

--- 1d-life/src/1d-life.js
+++ 1d-life/src/1d-life.js
@@ -39,18 +39,11 @@
    * @param {*} x the current x coordinate in question
    */
   function getNewVal(lifeState, x) {
-    // !!! IMPLEMENT ME
-    if (x < 1) {
-      if (lifeState[x+1] === 1) return 1;
-    }
-    else if (x === lifeState.length) {
-      if (lifeState[x-1] === 1) return 1;
-    }
-    else if (x >= 1 && x < lifeState.length) {
-      if (lifeState[x-1] === 1 && lifeState[x+1] === 1) return 0;
-      if (lifeState[x-1] === 1 || lifeState[x+1] === 1) return 1;
-    }
-    return 0; // instead of this
+    let zeroOrOne = 0;
+    x < 1 && lifeState[x+1] === 1 ? zeroOrOne = 1 : null;
+    x === lifeState.length && lifeState[x-1] === 1 ? zeroOrOne = 1 : null;
+    (x >= 1 && x < lifeState.length) && (lifeState[x-1] === 1 || lifeState[x+1] === 1 ) && !(lifeState[x-1] === 1 && lifeState[x+1] === 1) ? zeroOrOne = 1 : null;
+    return zeroOrOne;
   }
 
   /**
