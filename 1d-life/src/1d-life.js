81e2af7c5467a0e81bb882c9d1d100a5ae8f781e
perryahern <pahern@gmail.com>
2018-06-15 13:43:40 -0400

    Practical Security completed

--- 1d-life/src/1d-life.js
+++ 1d-life/src/1d-life.js
@@ -39,7 +39,7 @@
    * @param {*} x the current x coordinate in question
    */
   function getNewVal(lifeState, x) {
-  //   // Check if the cell on either side is out of bounds and handle it
+    // Check if the cell on either side is out of bounds and handle it
     if (x < 1 || x >= lifeState.length - 1) {
       if ((x < 1) && (lifeState[x] === 1 || lifeState[x + 1] === 1)) return 1;
       // return ((x >= lifeState.length - 1) && (lifeState[x] === 1 || lifeState[x - 1] === 1)) ? 1 : 0;  // this could replace following 2 lines
@@ -50,6 +50,18 @@
     // Return zero if all three cells are the same, else return 1
     return ((lifeState[x - 1] === lifeState[x]) && (lifeState[x + 1] === lifeState[x])) ? 0 : 1;
   }
+
+  // Saving for demo purposes, ignore this
+  // function getNewVal(lifeState, x) {
+  //   // Check if the cell on either side is out of bounds and handle it
+  //   if (x < 1 || x >= lifeState.length - 1) {
+  //     // if tests written incorrectly
+  //     // else return 0;
+  //   }
+
+  //   // Return zero if all three cells are the same, else return 1
+  //   return ((lifeState[x - 1] === lifeState[x]) && (lifeState[x + 1] === lifeState[x])) ? 0 : 1;
+  // }
 
   /**
    * Draw life
---
+++ security/PracticalSecurity.md
@@ -0,0 +1,7 @@
+Regarding my own security, I do keep my devices locked and use two-factor authentication on my accounts. I also use a password manager, and when possible I log in to services with Google's authentication.
+
+I need to update some of the passwords on a few accounts that are stored in my password manager because I do still have some old, less secure passwords that had been used on multiple accounts before I started using the password manager.
+
+I also need to check some of my network equipment and make sure it's patched and up to date.
+
+I've taken some steps today to update some of these things, and will continue to do so.
